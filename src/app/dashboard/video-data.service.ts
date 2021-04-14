05d246b82d680e713e2805ce789551f3eface187
Jay Siegel <jay.siegel1@gmail.com>
2021-04-14 16:16:40 -0400

    added missing method to video-data.service

--- src/app/dashboard/video-data.service.ts
+++ src/app/dashboard/video-data.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Video } from "./types";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class VideoDataService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getVideos(): Observable<Video[]> {
+    return this.http
+      .get<Video[]>("https://api.angularbootcamp.com/videos")
+      .pipe(
+        map((videos) =>
+          videos.map((video) => ({
+            ...video,
+            title: video.title.toUpperCase(),
+          }))
+        )
+      );
+  }
 }
