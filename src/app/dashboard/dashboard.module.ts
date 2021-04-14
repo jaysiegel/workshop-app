1feb0cca7e2acc28783be58558a4dae0f2fb0ca1
Jay Siegel <jay.siegel1@gmail.com>
2021-04-14 16:33:16 -0400

    Step 401 changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
-import { VideoListComponent } from './video-list/video-list.component';
-import { VideoPlayerComponent } from './video-player/video-player.component';
-import { StatFiltersComponent } from './stat-filters/stat-filters.component';
-import { VideoItemComponent } from './video-item/video-item.component';
+import { VideoDashboardComponent } from "./video-dashboard/video-dashboard.component";
+import { VideoListComponent } from "./video-list/video-list.component";
+import { VideoPlayerComponent } from "./video-player/video-player.component";
+import { StatFiltersComponent } from "./stat-filters/stat-filters.component";
+import { VideoItemComponent } from "./video-item/video-item.component";
 
-const routes: Routes = [{ path: '', component: VideoDashboardComponent }];
+const routes: Routes = [{ path: "", component: VideoDashboardComponent }];
 
 @NgModule({
   declarations: [
@@ -18,6 +19,6 @@ const routes: Routes = [{ path: '', component: VideoDashboardComponent }];
     StatFiltersComponent,
     VideoItemComponent,
   ],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
 })
 export class DashboardModule {}
--- src/app/dashboard/stat-filters/stat-filters.component.ts
+++ src/app/dashboard/stat-filters/stat-filters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-stat-filters',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stat-filters.component.scss']
 })
 export class StatFiltersComponent implements OnInit {
+  group: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.group = fb.group({
+      region: ['all'],
+      fromDate: ['2000-01-01'],
+      toDate: ['2022-01-01'],
+      under18: [true],
+      '18to40': [true],
+      '40to60': [true],
+      over60: [true]
+    });
+  }
 
   ngOnInit(): void {
   }
