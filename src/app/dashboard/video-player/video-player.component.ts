e15e79511c3c3c648103ac878a36b312b169cf46
Jay Siegel <jay.siegel1@gmail.com>
2021-04-15 12:43:58 -0400

    Step 718

--- src/app/dashboard/video-player/video-player.component.ts
+++ src/app/dashboard/video-player/video-player.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Video } from '../types';
+import { Component, Input, OnInit } from "@angular/core";
+import { SafeResourceUrl, DomSanitizer } from "@angular/platform-browser";
+import { Video } from "../types";
 
 @Component({
-  selector: 'app-video-player',
-  templateUrl: './video-player.component.html',
-  styleUrls: ['./video-player.component.scss']
+  selector: "app-video-player",
+  templateUrl: "./video-player.component.html",
+  styleUrls: ["./video-player.component.scss"],
 })
 export class VideoPlayerComponent implements OnInit {
-  @Input() video?: Video;
+  title = "";
+  iframeUrl?: SafeResourceUrl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() set video(v: Video | undefined) {
+    if (v) {
+      this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        `https://www.youtube.com/embed/${v.id}`
+      );
+      this.title = v.title;
+    }
   }
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {}
 }
